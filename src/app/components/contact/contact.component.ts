8cd4dc6e5d914ecc4cac9d57e26cd1bd24b0344b
Victor Ramirez <victorramirez@MacBook-Air-de-Victor.local>
2024-04-11 11:43:15 +0100

    WIP: Contact form style

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, FormsModule, Validators, ReactiveFormsModule } from '@angular/forms';
+import { ButtonComponent } from '../shared/button/button.component';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, ReactiveFormsModule, ButtonComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  contactForm = new FormGroup({
+      name: new FormControl(''),
+      email: new FormControl(''),
+      organization: new FormControl(''),
+      services: new FormControl(''),
+      message: new FormControl('')
+  });
+  
+  submitForm() {
+    console.log("SUBMIT")
+  }
 }
